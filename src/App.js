258e3e4981c701dbe006833d17938223d137fbfa
Thomas <65050898+tvonment@users.noreply.github.com>
2021-11-15 21:41:57 +0000

    initial commit / import material ui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return <div>
+    <div>
+      <h1>Hello!</h1>
+    </div>
+    <Button variant="contained">Button</Button>
+  </div>;
+}
+
+export default App;
